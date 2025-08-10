fb4667cea6f8ba8e9d413317c1c804a450ad8b37
suyungking <105546111+suyungking@users.noreply.github.com>
2025-08-10 18:21:00 +0900

    router, pinia 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import { createPinia } from 'pinia'
+import piniaPersistedstate from 'pinia-plugin-persistedstate'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPersistedstate)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
---
+++ frontend/src/router/index.js
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 페이지 컴포넌트(예시)
+
+const router = createRouter({
+
+})
+
+export default router
