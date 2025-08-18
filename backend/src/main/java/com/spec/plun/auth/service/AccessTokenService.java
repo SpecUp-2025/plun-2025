130b62847d6b529c92231f0b0a43fc4015ec88f3
suyungking <105546111+suyungking@users.noreply.github.com>
2025-08-18 19:01:14 +0900

    feat : access 토큰 서비스 구분

---
+++ backend/src/main/java/com/spec/plun/auth/service/AccessTokenService.java
@@ -0,0 +1,34 @@
+package com.spec.plun.auth.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.spec.plun.auth.util.JwtUtil;
+
+import io.jsonwebtoken.Claims;
+
+@Service
+public class AccessTokenService {
+	
+	private final JwtUtil jwtUtil;
+	
+	public AccessTokenService(@Qualifier("accessJwtUtil") JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+	
+	public String generateToken(String email) {
+		 Map<String, Object> claims = Map.of(
+		            "email", email);
+		 Date twoMinites = Date.from(Instant.now().plus(2,ChronoUnit.MINUTES));
+		 return jwtUtil.generateToken(claims, twoMinites);
+	}
+	
+	public Claims validToken(String accessToken) {
+		return jwtUtil.validToken(accessToken);
+	}
+}
