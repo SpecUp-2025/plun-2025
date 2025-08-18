858c86a35f6eb08e392e8d243eba0b6f32678e65
suyungking <105546111+suyungking@users.noreply.github.com>
2025-08-18 18:58:50 +0900

    feat : access,refresh 토큰 서비스로 구분화

--- backend/src/main/java/com/spec/plun/auth/service/AuthService.java
+++ backend/src/main/java/com/spec/plun/auth/service/AuthService.java
@@ -1,10 +1,5 @@
 package com.spec.plun.auth.service;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,22 +7,15 @@ import org.springframework.web.server.ResponseStatusException;
 import com.spec.plun.auth.DTO.LoginDTO;
 import com.spec.plun.auth.DTO.QualificationCheckDTO;
 import com.spec.plun.auth.DTO.TokenResponse;
-import com.spec.plun.auth.util.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-	
-	
-	@Qualifier("accessJwtUtil")
-	  private final JwtUtil accessJwt;
 
-	  @Value("${jwt.access-exp-seconds:1800}")
-	  private long accessTtlSec;
-	  
-	
+	private final AccessTokenService accessTokenService;
+	private final RefreshTokenService refreshTokenService;
 	private final QualificationService qualificationCheckService;
 
 	public TokenResponse login(LoginDTO member) {
@@ -36,15 +24,10 @@ public class AuthService {
 		if(qualification == null || !qualification.getPassword().equals(member.getPassword())) {
 			 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"아이디 혹은 비밀번호가 틀렸습니다.");
 		}
-		var roles = List.of("ROLE_USER");
-	    int minutes = (int)(accessTtlSec / 60);
-
-	    String access = accessJwt.generateToken(
-	      Map.of("sub", qualification.getEmail(), "roles", roles),
-	      minutes
-	    );
-
-	    return new TokenResponse("Bearer", access, accessTtlSec);
+	    return new TokenResponse(
+	    		accessTokenService.generateToken(qualification.getEmail()),
+	    		refreshTokenService.generateToken(qualification.getEmail())
+	    		);
 	}
 
 }
