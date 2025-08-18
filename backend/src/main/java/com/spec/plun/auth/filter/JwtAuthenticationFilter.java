8fab9e8d38499b7f941e887dede57864d8dcdfef
suyungking <105546111+suyungking@users.noreply.github.com>
2025-08-18 18:59:54 +0900

    feat : 토큰 필터로 유효성 검사

---
+++ backend/src/main/java/com/spec/plun/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.spec.plun.auth.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.spec.plun.auth.service.AccessTokenService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+	private final AccessTokenService accessTokenService;
+	
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		String path = request.getServletPath();
+		if("OPTIONS".equalsIgnoreCase(request.getMethod())) return true;
+		return path.startsWith("/auth");
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) 
+			throws ServletException,IOException{
+		String path = request.getHeader("Authorization");
+		
+		if(path == null || !path.startsWith("Bearer ")) {
+			chain.doFilter(request, response) ;
+			return ;
+		}
+		String token = path.substring(7).trim();
+		try {
+			Claims claims = accessTokenService.validToken(token);
+			String email = claims.get("email", String.class);
+			UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null,List.of());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+			
+		} catch (JwtException e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		}
+		chain.doFilter(request, response);
+	
+	}
+}
