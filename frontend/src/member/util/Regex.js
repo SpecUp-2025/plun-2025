a505a7750c802f9d073fef4757e263ccdb3ccb97
suyungking <105546111+suyungking@users.noreply.github.com>
2025-08-26 00:14:28 +0900

    feat : 검증 정규식 추가

---
+++ frontend/src/member/util/Regex.js
@@ -0,0 +1,4 @@
+export const REGEX_PATTERN = {
+    PASSWORD: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+    EMAIL: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+};
