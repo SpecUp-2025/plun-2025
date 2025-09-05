8efa64d6cfe8c66aebb42c5aec09777fcdca0d66
YONGMINJO1 <iy0594@naver.com>
2025-09-05 11:41:48 +0900

    calendar 화면 생성

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,6 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPersistedstate from 'pinia-plugin-persistedstate'
-// import '@fullcalendar/common/main.css'
-// import '@fullcalendar/daygrid/main.css'
 
 const app = createApp(App)
 
