d232d2f358545ffeae75dee549dbec4e32f3a8bc
suyungking <105546111+suyungking@users.noreply.github.com>
2025-09-12 00:52:24 +0900

    feat : 소셜 로그인시 받는 DTO 정의

---
+++ backend/src/main/java/com/spec/plun/auth/oauth2/DTO/UserInfo.java
@@ -0,0 +1,14 @@
+package com.spec.plun.auth.oauth2.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+	private String email;
+	private String name;
+
+}
